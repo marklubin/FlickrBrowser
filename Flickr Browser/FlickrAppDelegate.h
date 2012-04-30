c8cb9620f74546d1a8398c187db9faa77c62692c
Mark Lubin <melubin@gmail.com>
2012-04-30 17:36:39 -0400

    Initial Commit

---
+++ Flickr Browser/FlickrAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FlickrAppDelegate.h
+//  Flickr Browser
+//
+//  Created by Mark Lubin on 4/30/12.
+//  Copyright (c) 2012 Lubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Flickr Browser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Flickr Browser
+//
+//  Created by Mark Lubin on 4/30/12.
+//  Copyright (c) 2012 Lubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FlickrAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FlickrAppDelegate class]));
+    }
+}
